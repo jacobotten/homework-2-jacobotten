673584a2ae831acdaf66204bf46db4cf24d319f3
Jacob Otten <jtotten@ksu.edu>
2019-09-19 18:37:05 -0500

    commit changes to problem 2 and 3

--- count_files_and_subdirs.sh
+++ count_files_and_subdirs.sh
@@ -1,1 +1,9 @@
 #Counting files and subdirectories
+echo "There are "
+ls -1 | wc -l
+echo "files in this directory"
+
+echo " "
+echo "There are "
+ls -l | grep "^d" | wc -l
+echo "subdirectories in this directory"
--- largest_five.sh
+++ largest_five.sh
@@ -1,1 +1,2 @@
 #Prints the largest 5 files
+ls -Slhp | grep -v / | head -6
