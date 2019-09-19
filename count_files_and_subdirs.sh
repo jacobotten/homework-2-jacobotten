dcebe132d738831ebf3a510503869d87765c0fa8
Jacob Otten <jtotten@ksu.edu>
2019-09-19 18:53:47 -0500

    commit changes to 2

--- count_files_and_subdirs.sh
+++ count_files_and_subdirs.sh
@@ -1,9 +1,9 @@
 #Counting files and subdirectories
 echo "There are "
-ls -1 | wc -l
+ls -p | grep -v "/$" | wc -l
 echo "files in this directory"
 
 echo " "
 echo "There are "
-ls -l | grep "^d" | wc -l
+ls -l | grep ^d | wc -l
 echo "subdirectories in this directory"
