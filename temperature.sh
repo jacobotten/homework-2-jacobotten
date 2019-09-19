e15f75dae4a3bb53e260192e9eda60f723af7d03
Jacob Otten <jtotten@ksu.edu>
2019-09-19 18:19:40 -0500

    commit changes to temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -3,4 +3,4 @@ clear
 echo "Enter a Farenheight temperature to convert to Celsius:"
 read tempf
 echo "Temp in degrees Celsius: "
-echo "scale=3; (($tempf-32.0)*(5.0/9.0)) " | bc
+echo "scale=3; (($tempf-32.0)*(5.0/9.0))" | bc
